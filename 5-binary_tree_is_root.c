aefc7372d6e4a3d0653794026a5e4bfe59e9110d
daod2003 <1253@holbertonschool.com>
2020-03-13 19:05:09 +0000

    erase ; after the declaration the function

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -5,7 +5,7 @@
  * Return: 1 or 0
  **/
 
-int binary_tree_is_root(const binary_tree_t *node);
+int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (node != NULL && node->parent == NULL)
 		return (1);
