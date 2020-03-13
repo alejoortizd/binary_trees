ea509100ce21c912b9e69a0ec49faedbd18c9eb1
daod2003 <1253@holbertonschool.com>
2020-03-13 16:59:14 +0000

    add file 4-binary_tree_is_leaf and include function in the header

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_leaf - Entry point
+ * @node: pointer that point to the parent node
+ * Return: a pointer with the new node
+ **/
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node != NULL && node->right == NULL && node->left == NULL)
+		return (1);
+	return(0);
+}
