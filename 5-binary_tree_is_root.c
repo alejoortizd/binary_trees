77bf57469ae799a211fb9254ac17fba6e3e2b0fb
daod2003 <1253@holbertonschool.com>
2020-03-13 19:02:40 +0000

    add file 5-binary_tree_is_root and inlude the function in the header

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_root - Entry point
+ * @node: pointer that point to the parent node
+ * Return: 1 or 0
+ **/
+
+int binary_tree_is_root(const binary_tree_t *node);
+{
+	if (node != NULL && node->parent == NULL)
+		return (1);
+	return (0);
+}
