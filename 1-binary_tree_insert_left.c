119fd75d7ce9500706fd355000234c355bb24900
daod2003 <1253@holbertonschool.com>
2020-03-13 06:36:56 +0000

    modified conditions and context for the if in line 23

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -20,9 +20,11 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	newNode->n = value;
 	newNode->right = NULL;
 	newNode->left = NULL;
-	if (parent->left == NULL)
-		return (NULL);
-	newNode->left = parent->left;
+	if (parent->left != NULL)
+	{
+		newNode->left = parent->left;
+		newNode->left->parent = newNode;
+	}
 	parent->left = newNode;
 	return (newNode);
 }
