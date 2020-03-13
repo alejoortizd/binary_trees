a59498df8220fda44356ba5a337f2e17ce552069
daod2003 <1253@holbertonschool.com>
2020-03-13 06:29:25 +0000

    add file 1-binary_tree_insert_left and include the function for file 1

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_left - Entry point
+ * @parent: pointer that point to the parent node
+ * @value: the data inside the node
+ * Return: a pointer with the new node
+ **/
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newNode;
+
+	newNode = malloc(sizeof(binary_tree_t));
+	if (newNode == NULL)
+	{
+		free(newNode);
+		return (NULL);
+	}
+	newNode->parent = parent;
+	newNode->n = value;
+	newNode->right = NULL;
+	newNode->left = NULL;
+	if (parent == NULL)
+		return (NULL);
+	newNode->left = parent->left;
+	parent->left = newNode;
+	return (newNode);
+}
