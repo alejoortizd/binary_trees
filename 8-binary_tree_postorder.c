9e62c38369bd4df9de0550cd4ee30c36a55831f2
daod2003 <1253@holbertonschool.com>
2020-03-13 20:22:03 +0000

    add file 8-binary_tree_postorder.c and include the function in the header

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_order - Entry point
+ * @tree: pointer that point to the parent node
+ * @func: function that call all nodes
+ * Return: 1 or 0
+ **/
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+	binary_tree_inorder(tree->left, func);
+	binary_tree_inorder(tree->right, func);
+	func(tree->n);
+}
