32eb36b46bab7bdf7f0a6e8e7ea2cb54fa96f602
daod2003 <1253@holbertonschool.com>
2020-03-13 23:20:59 +0000

    add file 10-binary_tree_depth and include the function in the file header

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_height - Entry point
+ * @tree: pointer that point to the parent node
+ * Return: 1 or 0
+ **/
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t stree = 0;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->parent)
+		stree = binary_tree_height(tree->parent) + 1;
+	return (stree);
+}
