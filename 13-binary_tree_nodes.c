b73cd0c7c93604b8999fc336a8442c5aacbc6873
daod2003 <1253@holbertonschool.com>
2020-03-14 02:23:54 +0000

    add file 13-binary_tree_nodes and include the function in the header

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_leaves - Entry point
+ * binary_tree_nodes - Entry point
  * @tree: pointer that point to the parent node
  * Return: 1 or 0
  **/
@@ -13,6 +13,6 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 		return (0);
 	if (tree->left == NULL && tree->right == NULL)
 		return (0);
-	stree = binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right) + 1;
+	stree = binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1;
 	return (stree);
 }
