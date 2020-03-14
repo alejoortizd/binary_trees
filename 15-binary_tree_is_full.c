0f5d9759b58722390e7eaca567ff84e2b7397437
daod2003 <1253@holbertonschool.com>
2020-03-14 04:31:04 +0000

    add file 15-binary_tree_is_full and include the function in the header

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_full - Entry point
+ * @tree: pointer that point to the parent node
+ * Return: 1 or 0
+ **/
+
+int binary_tree_is_full(const binary_tree_t *tree)
+
+{
+	size_t stree = 0;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	stree = binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right);
+	return (stree);
+}
